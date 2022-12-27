c496abf0c3793174de6173f56f4384d5d4d5f9c1
gavinhsmith <39228744+gavinhsmith@users.noreply.github.com>
2022-12-26 23:52:22 -0600

    Further patched workflows + Fixed footer
    - This is a silly amount of changes
    - Fixed footer not fitting correctly
    - Hopefully fully fixed workflows again

--- pages/components/Layout/Footer.tsx
+++ pages/components/Layout/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
     return (
         <footer className='p-4 bg-dark text-light w-full flex flex-col md:flex-row items-center justify-center md:justify-between'>
             <p>&copy; 2022 Gavin Smith</p>
-            <div className='flex flex-row items-center align-center'>
+            <div className='flex flex-col md:flex-row items-center align-center'>
                 <ExternalLink href="mailto:gavinhsmith2005@gmail.com"><span className='m-1'>gavinhsmith2005@gmail.com</span></ExternalLink>
-                <span className='mx-3'>&#x2022;</span>
+                <span className='hidden md:block mx-3'>&#x2022;</span>
                 <ExternalLink href="https://github.com/gavinhsmith"><span className='m-1'>gavinhsmith @ GitHub</span></ExternalLink>
             </div>
         </footer>
