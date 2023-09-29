71043999210865de49c2fa032d14b375935ec7aa
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2023-09-29 06:11:52 -0500

    Change email to me@gavinhsmith.com

--- pages/components/Layout/Footer.tsx
+++ pages/components/Layout/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className='p-4 bg-dark text-light w-full flex flex-col md:flex-row items-center justify-center md:justify-between'>
             <p>&copy; 2022 Gavin Smith</p>
             <div className='flex flex-col md:flex-row items-center align-center'>
-                <ExternalLink href="mailto:gavinhsmith2005@gmail.com"><span className='m-1'>gavinhsmith2005@gmail.com</span></ExternalLink>
+                <ExternalLink href="mailto:me@gavinhsmith.com"><span className='m-1'>me@gavinhsmith.com</span></ExternalLink>
                 <span className='hidden md:block mx-3'><LoadingIcon /></span>
                 <ExternalLink href="https://github.com/gavinhsmith"><span className='m-1'>gavinhsmith @ GitHub</span></ExternalLink>
             </div>
