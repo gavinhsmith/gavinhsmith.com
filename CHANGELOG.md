849d629c6284b3a3e749b00141f38fd9394ded87
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-09-26 11:33:01 -0500

    Changed ver to 0.1.2 and updated copywrite

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.1.2
+
+Just updating some security stuff, and hopefully will be able to begin working on this project again soon.
+
 # v0.1.1
 
 This update makes the website mobile friendly (almost) in it's current state, and offically adds the fun console toy to mess with on the home page.
--- pages/components/Layout/Footer.tsx
+++ pages/components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ import LoadingIcon from "./LoadingIcon";
 export default function Footer() {
     return (
         <footer className='p-4 bg-dark text-light w-full flex flex-col md:flex-row items-center justify-center md:justify-between'>
-            <p>&copy; 2022 Gavin Smith</p>
+            <p>&copy; 2024 Gavin Smith</p>
             <div className='flex flex-col md:flex-row items-center align-center'>
                 <ExternalLink href="mailto:me@gavinhsmith.com"><span className='m-1'>me@gavinhsmith.com</span></ExternalLink>
                 <span className='hidden md:block mx-3'><LoadingIcon /></span>
